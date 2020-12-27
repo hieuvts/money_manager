772ea80f2b1ed4e0da7e9f03fa11134b8a11a43f
Hieu <17520482@gm.uit.edu.vn>
2020-12-27 19:16:23 +0700

    #6.2 Remove useless code

--- lib/main.dart
+++ lib/main.dart
@@ -56,8 +56,8 @@ class _PageControllerState extends State<PageController> {
   final List<Widget> _pages = <Widget>[
     TransactionPage(),
     GraphPage(),
-    //AddRecordPage(),
-    Dashboard(),
+    AddTransactionPage(),
+    AddTransactionPage(),
     SettingPage()
   ];
   int _selectedPage = 0;
@@ -70,32 +70,32 @@ class _PageControllerState extends State<PageController> {
       label: 'Giao dịch',
     ),
     BottomNavigationBarItem(
-        icon: new Image.asset('images/graph.png'), label: 'Thống kê'),
+        icon: new Image.asset(
+          'images/graph.png',
+        ),
+        label: 'Thống kê'),
     BottomNavigationBarItem(
-        icon: new Image.asset('images/add.png'), label: 'Thêm'),
+        icon: new Image.asset(
+          'images/add.png',
+        ),
+        label: 'Thêm'),
     BottomNavigationBarItem(
-        icon: new Image.asset('images/settings.png'), label: 'Cài đặt'),
+        icon: new Image.asset(
+          'images/settings.png',
+        ),
+        label: 'Cài đặt'),
   ];
+
   //Beginof Config SnakeNavBar
-  ShapeBorder bottomBarShape = new Border();
   SnakeBarBehaviour snakeBarStyle = SnakeBarBehaviour.floating;
   EdgeInsets padding = const EdgeInsets.all(5);
   SnakeShape snakeShape = SnakeShape.rectangle;
-  bool showSelectedLabels = false;
-  bool showUnselectedLabels = false;
   Color selectedColor = Colors.blueAccent[100];
   Gradient selectedGradient =
       const LinearGradient(colors: [Colors.red, Colors.amber]);
   Color unselectedColor = Colors.blueGrey;
   Gradient unselectedGradient =
       const LinearGradient(colors: [Colors.red, Colors.blueGrey]);
-  // Color containerColor;
-  // List<Color> containerColors = [
-  //   const Color(0xFFFDE1D7),
-  //   const Color(0xFFE4EDF5),
-  //   const Color(0xFFE7EEED),
-  //   const Color(0xFFF4E4CE),
-  // ];
   //Endof Config SnakeNavBar
 
   @override
@@ -109,19 +109,11 @@ class _PageControllerState extends State<PageController> {
       bottomNavigationBar: SnakeNavigationBar.color(
         behaviour: snakeBarStyle,
         snakeShape: snakeShape,
-        shape: bottomBarShape,
         snakeViewColor: selectedColor,
         selectedItemColor:
             snakeShape == SnakeShape.indicator ? selectedColor : null,
         unselectedItemColor: Colors.blueGrey,
-        showUnselectedLabels: showUnselectedLabels,
-        showSelectedLabels: showSelectedLabels,
         currentIndex: _selectedPage,
-        // onTap: (index) {
-        //   setState(() => _selectedPage = index);
-        //   onTabTapped(index);
-        //   print('tapped page $index');
-        // },
         onTap: onTabTapped,
         selectedLabelStyle: const TextStyle(fontSize: 10),
         items: navBarItems,
@@ -129,40 +121,7 @@ class _PageControllerState extends State<PageController> {
     ));
   }
 
-  void _onPageChanged(int page) {
-    //containerColor = containerColors[page];
-    switch (page) {
-      case 0:
-        setState(() {
-          snakeBarStyle = SnakeBarBehaviour.pinned;
-          snakeShape = SnakeShape.rectangle;
-          //showSelectedLabels = true;
-        });
-        break;
-      case 1:
-        setState(() {
-          snakeBarStyle = SnakeBarBehaviour.pinned;
-          snakeShape = SnakeShape.rectangle;
-        });
-        break;
-
-      case 2:
-        setState(() {
-          snakeBarStyle = SnakeBarBehaviour.pinned;
-          snakeShape = SnakeShape.rectangle;
-        });
-        break;
-      case 3:
-        setState(() {
-          snakeBarStyle = SnakeBarBehaviour.pinned;
-          snakeShape = SnakeShape.rectangle;
-        });
-        break;
-    }
-  }
-
   void onTabTapped(int index) {
-    _onPageChanged(index);
     setState(() {
       _selectedPage = index;
     });
